7ee4899ef0e78ea7d5435e692f8778fa6b70fb2a
Tomas Ahnfelt Rocha Martins <caradepato1@gmail.com>
2020-12-14 10:24:36 +0000

    Changed lista marcações in marcacoesRecholha.html to a table from a list and gave basic CSS style.

--- app/public/javascripts/listaMarcacoes.js
+++ app/public/javascripts/listaMarcacoes.js
@@ -4,18 +4,18 @@ window.onload = function(){
 
 async function loadMarcacoes() {
 	try{
+		let html = "<tr><th>data/hora</th><th>Nome do dador</th><th>Zona</th><th>Equipa de recolha</th></tr>";
 		let marcacoes = await $.ajax({
 			url: "/api/marcacoesRecolha",
 			method: "get",
 			dataType: "json"
 		});
-		let html = "";
 		for (let i=0; i < marcacoes.length; i++){
-			html += "<li><span>" + marcacoes[i].hora + "</span> <span>" + marcacoes[i].dador + "</span> <span>" + marcacoes[i].zona + "</span> <span>"+ marcacoes[i].equipa + "</span></li>";
+			html += "<tr><th>" + marcacoes[i].hora + "</th> <th>" + marcacoes[i].dador + "</th> <th>" + marcacoes[i].zona + "</th> <th>"+ marcacoes[i].equipa + "</th></tr>";
 		}
 		document.getElementById("marcacoes").innerHTML = html;
 	}	catch(err){
-		document.getElementById("marcacoes").innerHTML = "<li>Occureu um problema, se faz favor tentar mais tarde</li>";
+		document.getElementById("marcacoes").innerHTML = "<tr><th<Occureu um problema, se faz favor tentar mais tarde</th></tr>";
 		console.log(err);
 	}
 
