d41ec7602bf2d98282b7819be527bc2ac192a087
Tomas Ahnfelt Rocha Martins <caradepato1@gmail.com>
2020-12-14 10:11:38 +0000

    Fixed database connection issues and sql query syntax.

--- app/public/javascripts/listaMarcacoes.js
+++ app/public/javascripts/listaMarcacoes.js
@@ -3,16 +3,15 @@ window.onload = function(){
 }
 
 async function loadMarcacoes() {
-	console.log("hello");
 	try{
 		let marcacoes = await $.ajax({
 			url: "/api/marcacoesRecolha",
-			method: "/",
+			method: "get",
 			dataType: "json"
 		});
 		let html = "";
 		for (let i=0; i < marcacoes.length; i++){
-			html += "<li><span>" + marcacoes[i].data + "</span> <span>" + marcacoes[i].dador + "</span> <span>" + marcacoes[i].zona + "</span> <span>"+ marcacoes[i].equipa + "</span></li>";
+			html += "<li><span>" + marcacoes[i].hora + "</span> <span>" + marcacoes[i].dador + "</span> <span>" + marcacoes[i].zona + "</span> <span>"+ marcacoes[i].equipa + "</span></li>";
 		}
 		document.getElementById("marcacoes").innerHTML = html;
 	}	catch(err){
